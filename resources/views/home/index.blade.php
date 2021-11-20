8e142d9ea85b2b828686b67e54dfeaed08ef244c
miyamototaiyu <area.kantou8008@icloud.com>
2021-11-20 20:55:26 +0900

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>株式会社Daiken</title>
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <link rel="icon" type="image/png" href="img/favicon.png') }}">
+  <link rel="stylesheet" media="all" href="{{ asset('/css/ress.min.css') }}"/>
+  <link rel="stylesheet" media="all" href="{{ asset('/css/style.css') }}"/>
+  <script src="{{ asset('/js/jquery-2.1.4.min.js') }}"></script>
+  <script src="{{ asset('/js/style.js') }}"></script>
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" href="img/favicon.png') }}"> </head>
+<body>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col span-12 header">
+          <h1><a href="">株式会社Daiken</a></h1>
+          <!-- <div class="header-box"><a href=""><span class="contact-button">お問い合わせ</span></a></div> -->
+        </div>
+      </div>
+      <div class="row">
+        <div class="col span-12">
+          <nav>
+            <div id="open"></div>
+            <div id="close"></div>
+            <div id="navi">
+              <ul>
+                <li><a href="#">ホーム</a></li>
+                <li><a href="#1">商品について</a></li>
+                <li><a href="#2">アクセス</a></li>
+                <li><a href="#3">お問い合わせ</a></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="mainimg"> <img src="{{ asset('/images/mainimg.jpg') }}" alt="メイン画像"> </div>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col span-12">
+          <div class="catch">
+            <h2><span class="under">会社案内</span></h2>
+            <p>
+              株式会社Daikenは建築総合塗装サービスを提供する建築会社としてお客様のニーズを考慮し<br>
+              住宅および商業施設の塗装や改修工事を2014年の創業以来東京都足立区を拠点に提供してきました。<br>
+              どのようなご要望であれ皆様のお過ごしの空間を快適にできるよう努めております。<br>
+              ご検討いただけましたら、お電話またはメールにてお問い合わせください。
+            </p>
+            <!-- <p>ここにキャッチコピーが入ります。<br>お店の宣伝やコンセプトなど。魅せる文言を入力。</p> -->
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section id="1" class="gray-back">
+      <h2 class="center"><span class="under">各種サービス</span></h2>
+      <div class="container center">
+        <div class="row">
+          <div class="col span-4"> <img src="{{ asset('/images/product.jpg') }}" alt="ここに商品">
+            <h3>総合塗装工事</h3>
+            <p>
+              外壁の塗り替え・屋根塗り替え・内装塗装・屋上防水工事など総合塗装工事を一括でご依頼いただけます。
+              建物の外壁材や屋根材は、雨風や紫外線の影響を受けることで徐々に劣化していきます。
+              外壁・屋根の傷みは住宅そのものの劣化に繋がりますので、定期的なメンテナンスが必要です。
+              「業者に建物の塗り替えをお願いしたい」そう感じた際はぜひ当社へご依頼ください。
+              お客様のご資産を丁寧に施工いたします。
+            </p>
+          </div>
+          <div class="col span-4"> <img src="{{ asset('/images/product2.jpg') }}" alt="ここに商品">
+            <h3>足場仮設</h3>
+            <p>
+              弊社では工事に必要な足場の仮設も行っており塗装・防水・シーリング・足場設置までを一括してご依頼いただけます。
+              一式で受注させて頂けるためスピーディーに施工に取り掛かれますので仕様変更にも柔軟に対応できます。
+              また弊社では足場設置のみのご依頼も承っております。
+              足場について詳しく知りたい方はまずはお気軽にご相談ください。
+            </p>
+          </div>
+          <div class="col span-4"> <img src="{{ asset('/images/product3.jpg') }}" alt="ここに商品">
+            <h3>防水・シーリング</h3>
+            <p>
+              屋根などは雨風が侵入し劣化が著しく進む部分であり、定期的なメンテナンスが必要となります。
+              弊社では各種シーリング工事・各種防水工事に対応しております。
+              ​修繕時期がわからない方、シーリング・防水工事のみのご依頼も承っております。
+              まずはお気軽にご相談ください。​
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="2">
+      <h2 class="center"><span class="under">アクセス</span></h2>
+      <div class="container">
+        <div class="row">
+          <div class="col span-12">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3237.5425975562407!2d139.81347155131542!3d35.762045680078245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188fcaed136973%3A0xcc0dcd189d1c4bc1!2z44CSMTIwLTAwMTQg5p2x5Lqs6YO96Laz56uL5Yy66KW_57a-54Cs77yT5LiB55uu77yY4oiS77yRIOODq-ODnuODvOOCuOODpee2vueArA!5e0!3m2!1sja!2sjp!4v1637298828579!5m2!1sja!2sjp" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="3">
+      <h2 class="center"><span class="under">お問い合わせ</span></h2>
+      <div class="container">
+        <div class="row">
+          <div class="col span-6">
+            <div class="contact-box">
+              <p><img src="{{ asset('/images/tel.png') }}" alt="電話"></p>
+              <p>090-5210-4739</p>
+            </div>
+          </div>
+          <div class="col span-6">
+            <div class="contact-box">
+              <p><img src="{{ asset('/images/mail.png') }}" alt="Eメール"></p>
+              <p>k_mikami@daiken-g.jp</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col span-12">
+            <form method="post" action="">
+              <table class="table full-width">
+                <tbody>
+                  <tr>
+                    <th><label for="name">お名前</label></th>
+                    <td><input class="full-width" type="text" name="お名前" placeholder="名前を記入" id="exampNameInput"></td>
+                  </tr>
+                  <tr>
+                    <th><label for="email">メールアドレス</label></th>
+                    <td><input class="full-width" type="email" name="Email" placeholder="メールアドレスを記入" id="exampleEmailInput"></td>
+                  </tr>
+                  <tr>
+                    <th><label for="tel">お電話番号</label></th>
+                    <td><input class="full-width" type="tel" name="お電話番号" placeholder="お電話番号を記入" id="exampleTellInput"></td>
+                  </tr>
+                  <tr>
+                    <th><label for="exampleMessage">お問い合わせ内容</label></th>
+                    <td><textarea class="full-width textarea" name="お問い合わせ内容" placeholder="用件をご記入ください …" id="exampleMessage"></textarea></td>
+                  </tr>
+                </tbody>
+              </table>
+              <p class="center"><input class="button" type="submit" value="送 信"></p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col span-4">
+          <h4>株式会社Daiken</h4>
+        </div>
+        <div class="col span-8">
+          <div class="container">
+            <div class="row">
+              <div class="col span-4">
+                <p>住所</p>
+                <p>東京都足立区西綾瀬<br>3-8-1-605</p>
+              </div>
+              <div class="col span-4">
+                <p>電話</p>
+                <p>090-5210-4739</p>
+              </div>
+              <div class="col span-4">
+                <p>メール</p>
+                <p>k_mikami@daiken-g.jp</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <div class="copyright"> <a href="https://popo-design.net" target="_blank">Copyright © 2021 daiken-g.jp</a> </div>
+  <p id="pagetop"><a href="#">TOP</a></p>
+</body>
+</html>
